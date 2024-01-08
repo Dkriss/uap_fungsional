48cf83e68b832b6d5a1d9ad659f33c2fc4543a45
Dkriss <89033170+Dkriss@users.noreply.github.com>
2024-01-08 18:49:40 +0700

    Add files via upload

---
+++ uap.py
@@ -0,0 +1,79 @@
+# KODE NOMER 1 
+# LIST
+list1 = ['Pensil', 'Buku', 'Penggaris']
+list2 = [1500, 5000, 2000]
+
+print (type(list1))
+print (type(list2))
+
+#TUPLE
+tup1 = ('Pensil', 'Buku', 'Penggaris')
+tup2 = (1500, 5000, 2000)
+
+print(type(tup1))
+print(type(tup2))
+
+
+#KODE NOMER 2 Easy
+x = 13
+ is_prima(x)
+
+if x/2 = 0 > x:
+    print(type(True))
+else:
+    print(type(False))
+
+
+
+#KODE NOMER 3
+[1, 2, 3, 4, 5] total = 5
+
+ def tambah_angka(angka):
+    global total
+    total += angka
+    return total
+ 
+
+#KODE NOMER 4 Hard
+[ ] def klarifikasi_angka(x):
+    if x > 0:
+      return "Positif"
+    elif x < 0:
+      return "Negatif"
+    else:
+      return "Nol"
+    
+    def pencarian_angka(list_angka, find):
+      for num in list_angka:
+        if num == find:
+          return "found"
+        return "not found"
+
+
+#KODE NOMER 5 
+list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 
+        27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+for i in range(25) :
+  list.append(i)
+print(list)
+
+
+#KODE NOMER 6
+[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 
+        27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+def count (start=1, stop=5):
+  for i in range(start,stop+1):
+    yield i
+print(type(count))
+
+
+#KODE NOMER 7
+[ ] def sisagold(3,2):
+  return 3-2
+
+
+#KODE NOMER 8
+string = 'Ada apa dengan si Anna'
+data = ['ada', 'apa', 'dengan', 'si', 'anna']
+palindrome = ['ada', 'apa', 'anna']
+non-palindrome = ['dengan', 'si']
